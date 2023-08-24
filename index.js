7a6ac339d3621a49a718781eda1f8a768988292c
Achraf Mehanneche <achrafmehenneche.am@gmail.com>
2023-08-24 16:26:13 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// const wrapAdjective = function("*") {
+    
+// }
+
+const star = "*";
+// const adjective = "a hard worker";
+function wrapAdjective (star) {
+    
+    return function (adjective = "a hard worker") {
+        return `You are ${star}${adjective}${star}!`;
+    };
+}
+
+// const bigL = "||"
+// function wrapAdjective () {
+    
+//     return function (adjective = "a dedicated programmer") {
+//         return `You are ${bigL}${adjective}${bigL}!`;
+//     };
+// }
